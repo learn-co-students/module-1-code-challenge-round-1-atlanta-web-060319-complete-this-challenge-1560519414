a600134ee6d675630d917e47ab757730db9f561d
github username <github email address>
2019-06-14 10:59:32 -0400

    crush it

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,38 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
-  end
+      @@all <<  self
+    end
+  
+    def self.all
+        @@all
+    end
 
-  def full_name
-    "#{first_name} #{last_name}"
-  end
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+
+    def add_review(restaurant, rating, content)
+      Review.new(restaurant, self, rating, content)
+    end
+
+    def num_reviews
+      Review.all.select {|review| review.customer == self}.count
+    end
+
+    def restaurants
+      Review.all.map {|review| review.restaurant}.uniq
+    end
+
+  # def self.find_by_name(name)
+  #   self.all.select {|customer| customer.full_name == name}
+  # end
+
 end
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,15 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+chipotle = Restaurant.new("Chipotle")
+chopt = Restaurant.new("Chopt")
+
+cat = Customer.new("cat" , "cherubin")
+jack = Customer.new("jack", "jones")
+
+Review.new(chipotle, cat, 5, "yummmmmm")
+Review.new(chopt, cat, 5, "bbq")
+Review.new(chipotle, jack, 3, "rice n beans")
+Review.new(chopt, jack, 3, "salads r the best")
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
