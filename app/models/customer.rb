e941901dbbf89a250572d3345e6e918c4e7f4827
Brooke Jones <bjones782@gmail.com>
2019-06-14 11:02:36 -0400

    slayed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,56 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end 
+
+  # becomes a helper method for find_by_name(name)
   def full_name
     "#{first_name} #{last_name}"
   end
-end
+  
+
+  def reviews
+    Review.all.select do |rev| rev.customer == self 
+    end 
+  end 
+   
+  # Returns a **unique** array of all restaurants a customer has reviewed
+  def restaurants
+    reviews.map do |review| review.restaurant
+    end 
+  end 
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end 
+
+  def num_reviews
+    self.reviews.count 
+  end 
+
+  def self.find_by_name(full_name)
+    self.all.find do |customer|
+      customer.full_name == full_name
+    end 
+  end
+
+  def self.find_all_by_first_name(first_name)
+    cust = self.all.select do |cust| cust.first_name == first_name 
+    end
+  end  
+
+  def self.all_names
+    self.all.map {|customer| customer.full_name}
+  end 
+
+end 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,47 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
-end
+  def self.all
+    @@all
+  end 
+
+  def reviews
+    Review.all.select do |review| review.restaurant == self 
+    end 
+  end 
+   
+  def customers
+    reviews.map do |review| review.customer
+    end 
+  end 
+
+  def average_star_rating
+    total = 0
+    num_of_ratings = 0
+    reviews.collect { |rev|
+      total += rev.rating 
+      num_of_ratings += 1
+    } 
+    total/num_of_ratings
+  end 
+
+  def longest_review
+    reviews.max_by {|rev| rev.content.length}
+  end 
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name == name
+    end 
+  end
+
+
+
+end 
